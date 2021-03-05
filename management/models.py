0ce4ce9502c32da42afeaff15dbd7749fa53040d
yuuki291 <yuuki1948@outlook.jp>
2021-03-05 14:32:00 +0900

    更新

--- management/models.py
+++ management/models.py
@@ -24,4 +24,43 @@ class MstUsers(models.Model):
     def __str__(self):
         return self.UserName
 
-   # CompanyIdがnullなので後から修正
+
+# 勤務表
+class Roster(models.Model):
+    Date = models.DateField()
+    Work_Status = models.CharField(max_length=20)
+    CHOICES = (
+        ('Paid', '有給'),
+        ('Paid_AM_PM', '有給(午前・午後)'),
+        ('Paid_hours', '有給(時間)'),
+        ('Substitute_holiday', '代休'),
+        ('Transfer_holiday', '振替休'),
+    )
+    Application = models.CharField(max_length=20, choices=CHOICES)
+    Arrive_At_Work = models.DateTimeField()
+    Leave_The_Company = models.DateTimeField()
+    Man_Hours = models.IntegerField()
+    Remarks = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.Work_Status
+
+
+# CompanyIdがnullなので後から修正
+
+# ユーザ詳細
+class UserDetails(models.Model):
+    User_No = models.AutoField(auto_created=True, primary_key=True, serialize=False)
+    User_Name = models.CharField(blank=True, null=True, max_length=50, db_index=True, unique=True)
+    User_Name_kana = models.CharField(blank=True, null=True, max_length=50, db_index=True)
+    Gender = models.CharField(max_length=2, blank=True, null=True)
+    Birthday = models.DateField(blank=True, null=True)
+    Hire_Date = models.DateField(blank=True, null=True)
+    Type_Of_Contract = models.BooleanField(blank=True, null=True, max_length=15, db_index=True)
+    Affiliation_Department = models.BooleanField(blank=True, null=True, max_length=15, db_index=True)
+    Affiliation_Section = models.BooleanField(blank=True, null=True, max_length=15, db_index=True)
+    Position = models.CharField(blank=True, null=True, max_length=10, db_index=True)
+    Notices = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.User_Name
